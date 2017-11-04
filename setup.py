4eed5dad7ae8c471f4ef4be4bff2ff8f2648bdac
Ubuntu <apiestas@apiestas-vm.uk4wjjtymvoenasjlshxlowdad.ax.internal.cloudapp.net>
2017-11-04 16:06:24 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from distutils.core import setup
 setup(
     name='apiestas',
     version='1.0',
-    packages=['', 'arbs', 'utils', 'server', 'spiders'],
-    package_dir={'': 'apiestas'},
+    packages=['apiestas'],
     url='localhost',
     license='MIT License',
     author='Fran Lozano,  Jorge Sánchez',
